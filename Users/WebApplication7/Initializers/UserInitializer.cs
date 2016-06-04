5b46e8a78ab3589e09b7e04ac64dc9ef9f5a41f0
olagit <olagit@users.noreply.github.com>
2016-06-04 20:14:35 +0200

    Add files via upload

---
+++ Users/WebApplication7/Initializers/UserInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication7.Context;
+using WebApplication7.Models;
+
+namespace WebApplication7.Initializers
+{
+    public class UserInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<UserContext>
+    {
+        protected override void Seed(UserContext context)
+        {
+        }
+    }
+}
