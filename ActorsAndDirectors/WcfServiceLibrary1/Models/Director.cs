761c896e3f3c5f067909456a99d78bd63a31b796
Ola Pie <olaapierzchala@gmail.com>
2016-06-05 18:01:36 +0200

    datacontract, datamember

--- ActorsAndDirectors/WcfServiceLibrary1/Models/Director.cs
+++ ActorsAndDirectors/WcfServiceLibrary1/Models/Director.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WcfServiceLibrary1.Models
 {
+    [DataContract]
     class Director
     {
         [Key]
         public int PersonId { get; set; }
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public string Gender { get; set; }
+        [DataMember]
         public string RealName { get; set; }
+        [DataMember]
         public string MaritalStatus { get; set; }
+        [DataMember]
         public int Height { get; set; }
+        [DataMember]
         public string PlaceOfBirth { get; set; }
+        [DataMember]
         public DateTime DateOfBirth { get; set; }
+        [DataMember]
         public bool Alive { get; set; }
 
         [ForeignKey("Movie")]
